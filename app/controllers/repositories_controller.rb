7b1a50203467aad3444ee388e44179788290fae1
lafesh <ginafedder@yahoo.com>
2019-01-21 17:50:53 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,16 @@
 class RepositoriesController < ApplicationController
   
   def index
+    resp = Faraday.get"https://api.github.com/user/repos", {}, {
+      Authorization: "token #{session[:token]}"
+    }
+    @results = JSON.parse(resp.body)
+
+    response = Faraday.get"https://api.github.com/user", {}, {
+      Authorization: "token #{session[:token]}"
+    }
+    b = JSON.parse(response.body)
+    session[:username] = b["login"]
   end
 
 end
