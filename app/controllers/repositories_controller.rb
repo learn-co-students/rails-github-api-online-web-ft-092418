1440796029f9534690f436ec67a6d6d7f93b4320
larryvsdata <larryvsdata@gmail.com>
2019-01-22 10:16:32 -0800

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,12 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+  #  binding.pry
+  response = Faraday.get "https://api.github.com/user/repos" , {} , {
+    Authorization: "token #{session[:token]}"
+  }
+  @results = JSON.parse(response.body)
+  #binding.pry
   end
 
 end
