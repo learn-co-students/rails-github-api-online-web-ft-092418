fc08adcd7dec598bd6d3e73445f90164d58cecaa
harryFBloch <harryfbloch@gmail.com>
2019-01-22 15:26:12 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :authenticate_user
 
   private
 
   def authenticate_user
+    unless logged_in?
+      client_id = "7cff632c83e3831a11bd"
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
+      redirect_to github_url
+    end
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,23 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    begin
+    resp = Faraday.get "https://api.github.com/user/repos" do |req|
+       req.body = { 'client_id': "7cff632c83e3831a11bd",'client_secret': "7d5adeb9b122e4081de9d1009bfd96b82cdc65e3"}
+       req.headers['Authorization'] =  "token " + session[:token]
+    end
+    body = JSON.parse(resp.body)
+    @repos = listRepos(body)
+    @user = body[0]["owner"]["login"]
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+  end
+
+  def listRepos(data)
+    return data.map{ |repo|
+      repo["name"]
+      }
   end
 
 end
